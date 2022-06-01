1f0c07b75b7b2f9a101d9e97751e6288273d5ecd
Mhiriel <106572114+Mhiriel@users.noreply.github.com>
2022-06-01 12:51:52 +0800

    Add files via upload

---
+++ LAB3.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+num_int = 144
+num_flo = 2.67
+
+num_new = num_int + num_flo
+
+print("datatype of num_int:",type(num_int))
+print("datatype of num_flo:",type(num_flo))
+
+print("Value of num_new:",num_new)
+print("datatype of num_new:",type(num_new))
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
